978ffeaad00a6fce9e18f9b1a69ce38383280521
John Nealy <john.k.nealy@gmail.com>
2021-03-29 20:03:48 +0000

    updated mixlist01

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -14,4 +14,4 @@ print("The last item in the list (state): " + my_list[2] )
 
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-print("Using " + new_list[5] +  " on ports " + str(new_list[0]) + ", " + new_list[1] + ", or " + str(new_list[2]) + " is not working  on " + new_list[3] + " or "+  new_list[4] )
+print(f"Using {new_list[5]} on ports  {new_list[0]} , {new_list[1]} , or  {new_list[2]}  is not working  on {new_list[3]}  or {new_list[4]}" )
