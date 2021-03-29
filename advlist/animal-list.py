104438b31f6835ec7760f29e8f11be633214aad8
John Nealy <john.k.nealy@gmail.com>
2021-03-29 20:46:53 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# create a list called list1
+animals = ['Fox','Fly','Ant','Bee','Cod','Cat','Dog','Yak','Cow','Hen','Koi','Hog','Jay','Kit']
+# display list
+print(f"Here is a list of {len(animals)} animals")
+print(animals,"\n")
+
+#sort in alphabetical orde:
+animals.sort()
+print("Now sorted....\n",animals)
