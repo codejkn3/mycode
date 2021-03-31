2481ae7aeddd9c76ed444e77315d69b164aaaaf3
John Nealy <john.k.nealy@gmail.com>
2021-03-31 19:35:31 +0000

    Python and physical interfaces - updated

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -2,13 +2,21 @@
 import netifaces
 print(netifaces.interfaces())
 
+def getIPaddress(ifacename):
+    if ifacename in netifaces.interfaces():
+        ifindex = netifaces.interfaces().index(ifacename)
+        #print(netifaces.ifaddresses(ifacename)[netifaces.AF_INET][0]['addr'])
+        return (netifaces.ifaddresses(ifacename)[netifaces.AF_INET][0]['addr'])
+
+
 for i in netifaces.interfaces():
     print('\n**************Details of Interface - ' + i + ' *********************')
     try: 
         print('MAC: ', end='')
         print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr'])
         print('IP: ', end='')
-        print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+        #print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+        print(getIPaddress(i))
     
     except:          # This is a new line
         print('Could not collect adapter information') # Print an error message
